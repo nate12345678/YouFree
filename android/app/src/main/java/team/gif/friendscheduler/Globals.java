1e1e2e19938d52527036bfc75e3550d38129c870
Nate Post <nate12345678nate@gmail.com>
2019-02-23 10:02:19 -0600

    Android: added basic logo and reconfigured to use updated api

--- android/app/src/main/java/team/gif/friendscheduler/Globals.java
+++ android/app/src/main/java/team/gif/friendscheduler/Globals.java
@@ -3,7 +3,7 @@ package team.gif.friendscheduler;
 import java.util.ArrayList;
 
 public final class Globals {
-    public static final String BASE_URL = "http://216.171.5.211:61235/api/v1";
+    public static final String BASE_URL = "http://216.171.4.52:8080/api/v1";
     static long token;
     static User user;
     static ArrayList<User> friendsList = new ArrayList<>();
