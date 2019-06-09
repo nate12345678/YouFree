464c5b3726f590ff81d3b44dc094aadc981a28c6
Nate Post <nate12345678nate@gmail.com>
2019-06-08 19:52:44 -0500

    Attempting to add https to app

--- android/app/src/main/java/team/gif/friendscheduler/Globals.java
+++ android/app/src/main/java/team/gif/friendscheduler/Globals.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public final class Globals {
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-    public static final String BASE_URL = "http://71.201.248.45:8080/api/v1";
+    public static final String BASE_URL = "https://71.201.248.45:8080/api/v1";
     static long token;
     static User user;
     static String enteredPass;
--- android/app/src/main/java/team/gif/friendscheduler/Login.kt
+++ android/app/src/main/java/team/gif/friendscheduler/Login.kt
@@ -2,7 +2,6 @@ package team.gif.friendscheduler
 
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -14,7 +13,16 @@ import android.widget.EditText
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_login.*
 import okhttp3.*
+import java.io.BufferedReader
 import java.io.IOException
+import java.io.InputStream
+import java.net.URL
+import java.security.KeyStore
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
 
 
 class Login : AppCompatActivity() {
@@ -185,6 +193,43 @@ class Login : AppCompatActivity() {
         confirmPassText.visibility = View.GONE
         emailLabel.visibility = View.GONE
         emailText.visibility = View.GONE
+
+
+        Thread {
+            val cf: CertificateFactory = CertificateFactory.getInstance("X.509")
+// From https://www.washington.edu/itconnect/security/ca/load-der.crt
+            val caInput: InputStream = applicationContext.assets.open("cert.pem")
+            val ca: X509Certificate = caInput.use {
+                cf.generateCertificate(it) as X509Certificate
+            }
+            Log.w("ca=", ca.subjectDN.toString())
+
+// Create a KeyStore containing our trusted CAs
+            val keyStoreType = KeyStore.getDefaultType()
+            val keyStore = KeyStore.getInstance(keyStoreType).apply {
+                load(null, null)
+                setCertificateEntry("ca", ca)
+            }
+
+// Create a TrustManager that trusts the CAs inputStream our KeyStore
+            val tmfAlgorithm: String = TrustManagerFactory.getDefaultAlgorithm()
+            val tmf: TrustManagerFactory = TrustManagerFactory.getInstance(tmfAlgorithm).apply {
+                init(keyStore)
+            }
+
+// Create an SSLContext that uses our TrustManager
+            val context: SSLContext = SSLContext.getInstance("TLS").apply {
+                init(null, tmf.trustManagers, null)
+            }
+
+// Tell the URLConnection to use a SocketFactory from our SSLContext
+            val url = URL(Globals.BASE_URL + "/hello")
+            val urlConnection = url.openConnection() as HttpsURLConnection
+            urlConnection.sslSocketFactory = context.socketFactory
+            val inputStream: InputStream = urlConnection.inputStream
+            Log.w("contents", inputStream.bufferedReader().use(BufferedReader::readText))
+
+        }.start()
 
         var prefs = getSharedPreferences("prefs", Context.MODE_PRIVATE)
         Globals.stayLoggedOn = prefs.getBoolean("stayLoggedOn", false)
