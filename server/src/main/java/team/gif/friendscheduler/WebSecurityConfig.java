8057a03892193d3a4e85d68963a027817a15a9d5
PatrickUbelhor <pubelhor@gmail.com>
2019-08-31 01:11:47 -0500

    Fixed non-GET requests suddenly being forbidden

--- server/src/main/java/team/gif/friendscheduler/WebSecurityConfig.java
+++ server/src/main/java/team/gif/friendscheduler/WebSecurityConfig.java
@@ -11,9 +11,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-			.anyRequest()
-			.permitAll();
+		http.authorizeRequests()
+				.anyRequest()
+				.permitAll()
+				.and()
+				.csrf().disable();
+//				.and()
+//				.requiresChannel()
+//				.anyRequest()
+//				.requiresSecure();
 	}
 }
