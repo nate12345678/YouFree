63409cae2d593fea2e8458bde881a8450ef2e49a
Connor Sutton <occidentalis920@gmail.com>
2020-02-29 16:14:29 -0600

    React Components

--- server/react/src/App.js
+++ server/react/src/App.js
@@ -1,38 +1,141 @@
 import React from 'react';
-import Login from "./Login";
-import ScheduleView from "./ScheduleView";
+import './App.css';
+import WeekView from './components/WeekView';
+import IntervalForm from './components/IntervalForm';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp'
+
+const url = "https://patrickubelhor.com/api/v1";
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      intervals: null,
+      token: 0,
+      user: {
+        id: 0,
+        username: '',
+        email: ''
+      }
+    }
+  }
 
-		this.state = {
-			login: <Login/>,
-			loggedIn: false, //true if successfully logged in
-			dummySchedule: [
-				[[1, 4], [6, 8], [9, 24]],
-				[[0, 3], [6, 8], [9, 24]],
-				[[1, 4], [6, 8], [9, 14], [16, 17]],
-				[[1, 4], [6, 8], [9, 24]],
-				[[1, 4], [6, 8], [9, 24]],
-				[[1, 4], [6, 8], [9, 24]],
-				[[1, 4], [6, 8], [9, 24]],
+  componentDidMount() {
+    this.login('n@p.com','q');
+    // this.fetchSchedule();
+  }
 
-			]
-		}
-	}
+  fetchSchedule = () => {
+    fetch(url + '/schedule/' + this.state.user.id, {
+      headers: {
+        'token': this.state.token
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({intervals: data});
+      });
+  }
 
-	render() {
-		let content = <Login/>
-		if (this.state.loggedIn) {
-			content = <ScheduleView intervals={this.state.dummySchedule} />
-		}
-		return(
-			<div>
-				{content}
-			</div>
-		);
-	}
+  fetchUser = () => {
+    fetch(url + '/user/', {
+      headers: {
+        'token': this.state.token
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({user: data});
+      });
+  }
+
+  deleteInterval = (id, event) => {
+    let ok = window.confirm("This interval will be deleted. Are you sure?");
+    if (ok) {
+      fetch(url + '/schedule/' + id, {
+        method: 'delete',
+        headers: {
+          'intervalId': id,
+          'token': this.state.token
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          this.setState({intervals: data});
+        });
+    }
+  }
+
+  addInterval = (interval) => {
+    fetch(url + '/schedule', {
+      method: 'put',
+      headers: {
+        'token': this.state.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(interval)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({intervals: data});
+      });
+  }
+
+  login = (email, password) => {
+    fetch(url + '/login', {
+      headers: {
+        'email': email,
+        'password': password
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          user: data,
+          token: data.id});
+        this.fetchSchedule();
+      });
+  }
+
+  signUp = (user) => {
+    fetch(url + '/user', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(response => response.json())
+      .then(data => {
+        alert("Created User: " + data.username + " with id " + data.id)
+        console.log(data);
+      });
+  }
+
+  render() {
+    let username = null
+    username = this.state.user == null ? '' : this.state.user.username;
+    
+    let content = null;
+    content = this.state.intervals == null ? <div/> : <WeekView intervals={this.state.intervals} cellAction={this.deleteInterval} />;
+    
+
+    return (
+    <div className="App">
+      <h2>Logged In: {username}</h2>
+      {content}
+      <IntervalForm submitAction={this.addInterval} />
+      <SignIn submitAction={this.login} />
+      <SignUp submitAction={this.signUp} />
+    </div>
+    );
+  }
 }
 
 export default App;
