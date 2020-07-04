a95ac559918a6a1a64bea4b7eba09cf5702071ad
Patrick Ubelhor <pubelhor@gmail.com>
2020-07-03 22:10:33 -0500

    Added tabs to header

--- electron/src/components/Header.js
+++ electron/src/components/Header.js
@@ -1,13 +1,18 @@
 import '../css/Header.css';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import {
 	AppBar,
+	Button,
 	Icon,
 	Toolbar,
 	Typography
 } from '@material-ui/core';
 
 function Header(props) {
+
+	const history = useHistory();
+
 
 	function invertTheme() {
 		const body = document.body;
@@ -24,6 +29,8 @@ function Header(props) {
 		<AppBar id="appBar" position="sticky">
 			<Toolbar>
 				<Typography id="home" variant="h5" color="inherit">You Free</Typography>
+				<Button id="tab-home" variant="contained" color="secondary" onClick={() => history.push('/')}>Home</Button>
+				<Button id="tab-search" variant="contained" color="secondary" onClick={() => history.push('/search')}>Add Friends</Button>
 				<Icon id="lightModeIcon" color="inherit" onClick={invertTheme}>cloud</Icon>
 			</Toolbar>
 		</AppBar>
