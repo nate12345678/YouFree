1f77dd86b88fc058eca73d2cb6d38bec126c423d
PatrickUbelhor <pubelhor@gmail.com>
2021-02-20 11:43:14 -0600

    Extracted ackNotifications to proper REST controller

--- server/src/main/java/team/gif/friendscheduler/controller/NotificationController.java
+++ server/src/main/java/team/gif/friendscheduler/controller/NotificationController.java
@@ -3,27 +3,27 @@ package team.gif.friendscheduler.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.simp.annotation.SubscribeMapping;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import team.gif.friendscheduler.exception.UnauthorizedException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import team.gif.friendscheduler.model.notification.FriendRequestNotification;
 import team.gif.friendscheduler.service.AuthService;
 import team.gif.friendscheduler.service.NotificationService;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping(value = "/api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 public class NotificationController {
 	
+	private static final Logger logger = LogManager.getLogger(NotificationController.class);
 	private final AuthService authService;
 	private final NotificationService notificationService;
-	private static final Logger logger = LogManager.getLogger(NotificationController.class);
 	
 	@Autowired
 	public NotificationController(
@@ -35,42 +35,19 @@ public class NotificationController {
 	}
 	
 	
-	/*
-	 * Here's a possible shape for the response to subscribing to notifications
-	 * Each notification should have its own ID
-	 * {
-	 *   type: "BACKLOG", // Can be "BACKLOG", "PENDING_FRIENDS", "HANGOUT_REQUESTS", etc
-	 *   pendingFriends: [
-	 *     {
-	 *       userId: 1243456, // Add any information necessary for client to display. May only need count for now.
-	 *       ...
-	 *       notificationId: 12555
-	 *     }
-	 *   ],
-	 *   hangoutRequests: [],
-	 *   messages: []
-	 * }
-	 *
-	 */
-	@SubscribeMapping("/queue/notifications/{userId}")
-	public List<FriendRequestNotification> subscribeToNotifications(
-			@DestinationVariable Long userId,
-			@Header String token
+	@GetMapping("/notifications")
+	public ResponseEntity<List<FriendRequestNotification>> getNotifications(
+			@RequestHeader("token") String token
 	) {
-		logger.info("Received notification subscription: '/queue/notifications/{}", userId);
-		
+		logger.info("Received getNotifications request");
 		authService.validateTokenString(token);
 		Long requesterId = authService.getUserIdFromToken(token);
+		List<FriendRequestNotification> notifications = notificationService.getAllNotificationsForUser(requesterId);
 		
-		// If the token doesn't match the specified userId, send UNAUTHORIZED response
-		if (!requesterId.equals(userId)) {
-			throw new UnauthorizedException();
-		}
-		
-		return notificationService.getAllNotificationsForUser(requesterId);
+		return ResponseEntity.ok(notifications);
 	}
 	
-
+	
 	@DeleteMapping("/notifications")
 	public ResponseEntity<Void> acknowledgeNotifications(
 			@RequestHeader("token") String token,
